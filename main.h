4a06fed9b92a6e12dc0370bc1eb124858bf6c237
callmeliuchu <19931108github>
2022-05-28 19:59:12 +0800

    练习c++第一天，生成图片

---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by 刘楚 on 5/28/22.
+//
+
+#ifndef LEARN_C_PLUS_BY_PROJECT_MAIN_H
+#define LEARN_C_PLUS_BY_PROJECT_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //LEARN_C_PLUS_BY_PROJECT_MAIN_H
